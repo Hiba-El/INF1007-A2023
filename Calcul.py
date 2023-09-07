3e0e74cf25de1c078bdf7e3a63ae522d00b5ca88
Hiba-El <hiba.el-moataqid@polymtl.ca>
2023-09-07 18:39:54 -0400

    envoi

---
+++ Calcul.py
@@ -0,0 +1,11 @@
+import math
+#TODO Affichez l'aire d'un cercle avec le rayon indiqué
+rayon = 4
+
+pi = 3.1416
+A = pi*((rayon)**2)
+print(A)
+
+
+#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Hiba El")
