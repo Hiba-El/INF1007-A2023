a1c4226b359611401c276c25d06d7d5b799f0188
Hiba-El <yourname@yourplace.org>
2023-10-08 17:54:40 -0400

    question25 reussie!

--- TP/TP2/TP2_grille.py
+++ TP/TP2/TP2_grille.py
@@ -116,12 +116,13 @@ def ajuster_position_pour_grille_circulaire(lig, col, dim_lig, dim_col):
     # : Ajuster la position (ligne, colonne) pour une grille circulaire en utilisant les dimensions de la grille.
     # Indice: Un modulo (%) peut être utile.
     if col >= dim_col:  #à droite
-        col = 0
+        col = col % dim_col
     elif col < 0: #à gauche
-        col = dim_col -1
-    elif lig >= dim_lig:
-        lig = 0
-    elif lig < 0:
-        lig = dim_lig - 1
+        col = col % dim_col
+    if lig >= dim_lig: #en bas
+        lig = lig % dim_lig
+    elif lig < 0: #en haut
+        lig = lig % dim_lig
     return (lig, col)
+
 
