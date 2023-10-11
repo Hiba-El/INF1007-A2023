c2562541bc51a8b41be6631a0cec1a34d428cdf7
Hiba-El <yourname@yourplace.org>
2023-10-11 13:43:24 -0400

    qst 27

--- Calcul.py
+++ Calcul.py
@@ -1,11 +1,13 @@
-import math
-#TODO Affichez l'aire d'un cercle avec le rayon indiqué
-rayon = 4
-
-#pi = 3,14
-A = math.pi *((rayon)**2)
-print(A)
-
-
-#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
-print('Hiba El')
+# import math
+# #TODO Affichez l'aire d'un cercle avec le rayon indiqué
+# rayon = 4
+#
+# #pi = 3,14
+# A = math.pi *((rayon)**2)
+# print(A)
+#
+#
+# #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+# print('Hiba El')
+a = 4%3
+print(a)
