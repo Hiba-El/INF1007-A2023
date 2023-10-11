a84e94906db253acf947dfabc1700d49f394fd79
Hiba-El <yourname@yourplace.org>
2023-10-11 17:10:25 -0400

    qst 27

--- TP/TP2/TP2_grille.py
+++ TP/TP2/TP2_grille.py
@@ -5,6 +5,8 @@ from TP2_animal import obtenir_age
 from TP2_animal import obtenir_jours_gestation
 from TP2_animal import definir_jours_gestation
 from TP2_animal import ajouter_energie
+from TP2_animal import definir_disponibilite
+from TP2_animal import incrementer_age
 from enum import Enum
 import random
 class Contenu(Enum):
@@ -220,7 +222,115 @@ def remplir_grille(grille, pourcentage_proie, pourcentage_predateur):
         grille['nb_predateurs'] = nb_predateurs
     return grille
 
+#definir_etat
+def definir_etat(grille, contenu: Contenu, ligne, col):
+    grille['matrice'][ligne][col]['etat'] = contenu
+    return grille
+
 #28
 def rendre_animaux_disponibles(grille):
-    # TODO: Parcourir chaque case de la grille et rendre tous les animaux disponibles (Booléen à True) pour la prochaine itération.
-    pass
+    nb_lig, nb_col = obtenir_dimensions(grille)
+    # Parcourir chaque case de la grille et rendre tous les animaux disponibles (Booléen à True) pour la prochaine itération.
+    for ligne in range(nb_lig):
+        for colonne in range(nb_col):
+            case = obtenir_case(grille, ligne, colonne)
+            if obtenir_animal(grille, ligne, colonne) != None:
+                if obtenir_disponibilite(case['animal']) is False:
+                    definir_disponibilite(case['animal'], True)
+            else:
+                continue
+
+#28
+def deplacer_animal(grille, ligne, col, animal):
+    # Trouver un voisin vide où déplacer l'animal, effectuer le déplacement et mettre à jour l'état
+    # et la disponibilité de l'animal. Utiliser "choix_voisin_autour", "definir_etat", "definir_animal",
+    # "definir_disponibilite" et "vider_case" pour réaliser ces étapes.
+    longueur, lig, colonne = choix_voisin_autour(grille, ligne, col, Contenu.VIDE)
+    definir_etat(grille, Contenu.PROIE, lig, colonne)                   #Contenu.  PROIE ou PREDATEUR ou VIDE
+    definir_animal(grille, animal, lig, colonne)
+    definir_disponibilite(animal, False)
+    vider_case(grille, ligne, col)
+    return grille
+
+#29
+def executer_cycle_proie(grille, ligne, col, animal):
+    # Gérer le cycle de vie d'une proie à une position donnée sur la grille.
+    # 1. Vieillir l'animal. Si l'âge dépasse MAX_AGE_PROIE, le retirer de la grille et décrémenter le compteur de proies.
+    incrementer_age(animal, NB_JRS_PUBERTE_PROIE)
+    if animal['age'] > MAX_AGE_PROIE:
+        vider_case(grille, ligne, col)
+        decrementer_nb_proies(grille)
+        #pk on sort pas de la fonction (return grille) pcq la proie fait rien si elle es morte
+    # 2. Si l'animal est en âge de se reproduire et a attendu suffisamment (NB_JRS_GESTATION_PROIE), tenter de générer un nouveau bébé proie.
+    #    Pour ce faire, chercher un voisin vide autour de la proie. Si un voisin est trouvé, créer un bébé proie et le placer dans la grille.
+    elif animal['age'] >= NB_JRS_PUBERTE_PROIE and animal['jrs_gestation'] >= NB_JRS_GESTATION_PROIE:
+        longueur, lig_voisin, col_voisin = choix_voisin_autour(grille, ligne, col, Contenu.VIDE)
+        if lig_voisin is not None and col_voisin is not None and grille['nb_proies'] < NB_MAX_PROIES:
+            bb_proie = creer_case(Contenu.PROIE, creer_animal())
+            definir_case(grille, bb_proie, lig_voisin, col_voisin)
+            incrementer_nb_proies(grille)
+        definir_jours_gestation(animal, 0)
+    # 3. Sinon, déplacer l'animal vers une case vide à proximité.
+    else:
+        long, lig_dispo, col_dispo = choix_voisin_autour(grille, ligne, col, Contenu.VIDE)
+        if lig_dispo or col_dispo is None:
+            return grille
+        else:
+            deplacer_animal(grille, lig_dispo, col_dispo, animal)
+    return grille
+
+#31
+def executer_cycle_predateur(grille, ligne, col, animal):
+    incrementer_age(animal, NB_JRS_PUBERTE_PRED)
+    if animal['energie'] < MIN_SANTE_PRED or animal['age'] > MAX_AGE_PRED:
+        vider_case(grille, ligne, col)
+        decrementer_nb_predateurs(grille)
+        return grille
+    long, lig_proie_voisin, col_proie_voisin = choix_voisin_autour(grille, ligne, col, Contenu.PROIE)
+    if lig_proie_voisin is not None and col_proie_voisin is not None:
+        definir_disponibilite(animal, False)
+        ajouter_energie(animal, AJOUT_ENERGIE)
+        definir_case(grille, creer_case(Contenu.PREDATEUR, animal), lig_proie_voisin, col_proie_voisin)
+        vider_case(grille, ligne, col)
+        decrementer_nb_proies(grille)
+        if animal['age'] >= NB_JRS_PUBERTE_PRED and animal['jrs_gestation'] >= NB_JRS_GESTATION_PRED:
+            definir_jours_gestation(animal, 0)
+            ligne , col = lig_proie_voisin, col_proie_voisin #reassigner a ligne, col la nouvelle position (ou il a mangee la proie)
+            long, lig_voisin, col_voisin = choix_voisin_autour(grille, ligne, col, Contenu.VIDE)
+            if lig_voisin is not None and col_voisin is not None:
+                definir_case(grille, creer_case(Contenu.PREDATEUR, creer_animal()), ligne, col)
+                incrementer_nb_predateurs(grille)
+    else:
+        animal['energie'] -= 1
+        long, lig_dispo, col_dispo = choix_voisin_autour(grille, ligne, col, Contenu.VIDE)
+        if lig_dispo or col_dispo is None:
+            return grille
+        else:
+            deplacer_animal(grille, lig_dispo, col_dispo, animal)
+    return grille
+
+#32
+def executer_cycle(grille):
+    rendre_animaux_disponibles(grille)
+    nb_lignes, nb_col = obtenir_dimensions(grille)
+    for ligne in range(nb_lignes):
+        for colonne in range(nb_col):
+            case = obtenir_case(grille, ligne, colonne)
+            if case['etat'] != Contenu.VIDE:
+                animal = obtenir_animal(grille, ligne, colonne)
+                if obtenir_disponibilite(animal) is True:
+                    if case['etat'] == Contenu.PROIE:
+                        executer_cycle_proie(grille, ligne, colonne, animal)
+                    else:
+                        executer_cycle_predateur(grille, ligne, colonne, animal)
+    return grille
+
+#33
+def simulation_est_terminee(grille):
+    # Vérifier si la simulation est terminée.
+    # Elle se termine lorsque le nombre de proies ou le nombre de prédateurs est égal à zéro.
+    if grille['nb_proies'] == 0 or grille['nb_predateurs'] == 0:
+        return True
+    else:
+        return False
+    # Renvoyer un booléen indiquant l'état de la simulation.
